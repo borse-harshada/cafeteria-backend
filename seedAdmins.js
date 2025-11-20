216c4c104d37ff3f07204381d75843b8a478cac9
nitin-c1 <chaudharinitin2006@gmail.com>
2025-11-21 03:23:53 +0530

    Fix Vercel routing paths

--- seedAdmins.js
+++ seedAdmins.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const Admin = require("./models/Admin");
 
-mongoose.connect("mongodb+srv://chaudharinitin_db_user:Nitin1234@cafemenu.0qyjl3a.mongodb.net/");
+mongoose.connect("mongodb+srv://borseharshu:Harshu123@cafeteriacluster.lrebooy.mongodb.net/?appName=cafeteriaCluster");
 
 async function seedAdmins() {
     const admins = [
         { email: "borseharshu27@gmail.com", password: "Harshu@123" },
         { email: "foodhub@gmail.com", password: "Foodhub@123" },
         { email: "manager@cafe.com", password: "manager123" },
-        { email: "chaudharinitin2006@gmail.com", password: "Nitin@1" }
+        { email: "chaudharinitin2006@gmail.com", password: "Nitin@123" }
     ];
 
     for (let a of admins) {
